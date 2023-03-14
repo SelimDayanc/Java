20b472dce830012a78b5169089359beba00df6c1
Muhammed Selim <95688540+SelimDayanc@users.noreply.github.com>
2023-03-14 22:57:23 +0300

    Delete Dneme directory

--- Dneme/src/dneme/Dneme.java
+++
@@ -1,8 +0,0 @@
-package dneme;
-import java.util.Scanner;
-public class Dneme{
-    public static void main(String[]args){
-        double a = 12123.74525;
-       System.out.printf("%4.2f tl",a);
-    }
-}
